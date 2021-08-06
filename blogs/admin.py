1a0eb896add96c06b835558a2f9444375cb65bf7
iamohmx <48460454+iamohmx@users.noreply.github.com>
2021-08-06 20:47:21 +0700

    Add files via upload

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
+class DesignPostAdmin(admin.ModelAdmin):
+    # create table 
+    list_display = ["id","name","category","author","desc"]
+    # pagination
+    list_per_page = 3
 
-admin.site.register(Post)
+    list_editable=["name"]
+
+    list_filter = ["category","name"]
+
+    search_fields=["name","category"]
+
+    fields = ["author","category","name","desc"]
+
+admin.site.register(Post,DesignPostAdmin)
